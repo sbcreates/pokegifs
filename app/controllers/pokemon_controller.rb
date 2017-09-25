abaeaf8a0d218c06bfa006a2b1c8ecfd0353de1b
sbcreates <drawsarahb@gmail.com>
2017-09-25 16:13:50 -0400

    began writing code to retrieve api data for show page up to parsed response body

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,12 +1,18 @@
 class PokemonController < ApplicationController
 
   def index
-    render json: {"message": "ok"}
+    render json: { "message": "ok!" }
   end
 
   def show
-    render json: {"message": "ok"}
-  end
+    # find the pokemon with the same id
+    id = params[:id]
+    # envoking key saved locally
+    api_key = ENV["GIHPY_KEY"]
+    response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{id}")
+    parsed_response = JSON.parse(response.body)
 
+    render json: { "message": "ok!" }
+  end
 
 end
